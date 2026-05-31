e0bc6244e773a81961a2987d8c4b4d2efb6145ec
Souvik Das <84220971+sdx747@users.noreply.github.com>
2026-05-31 19:42:35 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let min = prices[0]
+        let profit = 0
+
+        for (let i=0; i<prices.length; i++) {
+            min = Math.min(min, prices[i])
+            profit = Math.max(profit, prices[i]-min)
+        }
+
+        return profit
+    }
+}
