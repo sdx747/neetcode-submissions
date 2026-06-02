4dbefd6423e37fb8b7524527d2f3167e0a00c243
Souvik Das <84220971+sdx747@users.noreply.github.com>
2026-06-02 09:47:15 +0530

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        const brackets = {
+            ")" : "(",
+            "}" : "{",
+            "]" : "["
+        }
+
+        const stack = []
+
+        for (const x of s) {
+            if (brackets[x]) {
+                const last = stack.pop()
+                if (!last || last !== brackets[x]) return false
+            } else {
+                stack.push(x)
+            }
+        }
+
+        return stack.length === 0
+    }
+}
